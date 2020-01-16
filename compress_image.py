e878cee910279badcccf2d558ca35ccca03f27ca
Himanshu Keshri <thehimanshukeshri@gmail.com>
2020-01-16 17:56:38 +0530

    checkpoint

--- compress_image.py
+++ compress_image.py
@@ -1,17 +1,18 @@
 import os
-from PIL import Image 
+from PIL import Image
 
-def compress(img_path, new_height, comp_img_path):
+
+def compress(img_path, new_height, comp_fol_path):
     '''
-    Input : img_path, new_height, comp_img_path
+    Input : img_path, new_height, comp_fol_path
     Output : new file path
     '''
-    if not os.path.exists(comp_img_path):
-        os.makedirs(comp_img_path)
+    if not os.path.exists(comp_fol_path):
+        os.makedirs(comp_fol_path)
     fname = img_path.split("\\")[-1]
     img = Image.open(img_path)
     hpercent = (new_height / float(img.size[1]))
     wsize = int((float(img.size[0]) * float(hpercent)))
     img = img.resize((wsize, new_height), Image.ANTIALIAS)
-    img.save(os.path.join(comp_img_path, fname))
-    return os.path.join(comp_img_path, fname)
+    img.save(os.path.join(comp_fol_path, fname))
+    return os.path.join(comp_fol_path, fname)
--- image_to_tiles.py
+++ image_to_tiles.py
@@ -1,13 +1,14 @@
 import numpy as np
-from PIL import Image 
+from PIL import Image
 
-def makeTiles(img_path,size,ratio=1):
+
+def makeTiles(img_path, size, ratio=1):
     '''
     Input : image path, size (tuple), ratio (0<r<=1)
     size - size of each tiles
     ratio - overlapping ratio.
         1 = no overlapping
-        0 = same patch. 
+        0 = same patch.
         warning : ratio = 0 results to an infinite loop
     Output : List of numpy array of image tiles
     '''
@@ -18,18 +19,18 @@ def makeTiles(img_path,size,ratio=1):
     tw = size[1]
     img_height = img.shape[0]
     img_width = img.shape[1]
-    
+
     left = 0
     top = 0
     right = left + tw
     bottom = top + th
-    
+
     while(bottom <= img_height):
         while(right <= img_width):
-            im1 = imobj.crop((left,top,right,bottom))
+            im1 = imobj.crop((left, top, right, bottom))
             n = np.array(im1)
             res.append(n)
-            left = left + int(tw*ratio)
+            left = left + int(tw * ratio)
             right = left + tw
         left = 0
         right = left + tw
