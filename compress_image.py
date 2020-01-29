b484a75370e1b197b1a44512bac93053359c30db
Himanshu Keshri <thehimanshukeshri@gmail.com>
2020-01-29 22:36:14 +0530

    Compress image added inplace Removing folder option

--- compress_image.py
+++ compress_image.py
@@ -2,17 +2,16 @@ import os
 from PIL import Image
 
 
-def compress(img_path, new_height, comp_fol_path):
+def compress(img_path, new_height, inplace=True, new_img_path):
     '''
     Input : img_path, new_height, comp_fol_path
     Output : new file path
     '''
-    if not os.path.exists(comp_fol_path):
-        os.makedirs(comp_fol_path)
-    fname = img_path.split("\\")[-1]
     img = Image.open(img_path)
     hpercent = (new_height / float(img.size[1]))
     wsize = int((float(img.size[0]) * float(hpercent)))
     img = img.resize((wsize, new_height), Image.ANTIALIAS)
-    img.save(os.path.join(comp_fol_path, fname))
-    return os.path.join(comp_fol_path, fname)
+    if(inplace):
+    	new_img_path = img_path 
+    img.save(new_img_path)
+    return new_img_path
