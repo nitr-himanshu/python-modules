cff40a0f2d2875d2d4da0c21504773a3c2f58543
Himanshu Keshri <thehimanshukeshri@gmail.com>
2020-01-31 16:35:32 +0530

    Create black_and_white_image.py

---
+++ black_and_white_image.py
@@ -0,0 +1,11 @@
+import cv2
+
+
+def convertToBlackAndWhite(img_path, inplace=True, new_img_path=""):
+    originalImage = cv2.imread(img_path)
+    grayImage = cv2.cvtColor(originalImage, cv2.COLOR_BGR2GRAY)
+    (thresh, blackAndWhiteImage) = cv2.threshold(
+        grayImage, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+    if(inplace):
+        new_img_path = img_path
+    cv2.imwrite(new_img_path, blackAndWhiteImage)
