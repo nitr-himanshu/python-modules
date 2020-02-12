220d5892de662e8500cde41f91312f1e107196bc
Himanshu Keshri <thehimanshukeshri@gmail.com>
2020-02-12 10:06:27 +0530

    Update black_and_white_image.py

--- black_and_white_image.py
+++ black_and_white_image.py
@@ -1,7 +1,14 @@
 import cv2
 
-
 def convertToBlackAndWhite(img_path, inplace=True, new_img_path=""):
+    '''
+    :param
+        img_path
+        [inplace] (default=True)
+        [new_img_path] (required when inplace=false)
+    :return
+        new file path
+    '''
     originalImage = cv2.imread(img_path)
     grayImage = cv2.cvtColor(originalImage, cv2.COLOR_BGR2GRAY)
     (thresh, blackAndWhiteImage) = cv2.threshold(
@@ -9,3 +16,4 @@ def convertToBlackAndWhite(img_path, inplace=True, new_img_path=""):
     if(inplace):
         new_img_path = img_path
     cv2.imwrite(new_img_path, blackAndWhiteImage)
+    return new_img_path
